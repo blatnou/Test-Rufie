670e8b68cf8b4049d32401850845c24df6fe5ad4
blatnou <78293774+blatnou@users.noreply.github.com>
2022-04-10 15:25:34 +0600

    Add files via upload

---
+++ sits.py
@@ -0,0 +1,10 @@
+from kivy.uix.label import Label
+from kivy.clock import Clock
+
+class Sits(Label):
+    
+    def __init__(self, total, **kwargs):
+        def 
+
+    def next(self, *args):
+        pass
